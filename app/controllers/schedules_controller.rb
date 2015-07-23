e13beda6288ee5cf04d1031b5495e4ad39f27ea0
alecorleonis <alejandramleon@unitec.edu>
2015-07-22 23:30:50 -0600

    adicion de helpers y validaciones por rol

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -5,7 +5,7 @@ class SchedulesController < ApplicationController
   # GET /schedules.json
   def index
 		if current_user
-		if current_user.id == 1
+		if current_user.role_id == 1 || current_user.role_id == 2
 	    @schedules = Schedule.all
 		else
 			redirect_to requests_path
@@ -23,7 +23,7 @@ class SchedulesController < ApplicationController
   # GET /schedules/new
   def new
 		if current_user
-		if current_user.id == 1
+		if current_user.role_id == 1 || current_user.role_id == 2
     	@schedule = Schedule.new
 		else
 			redirect_to requests_path
@@ -40,7 +40,7 @@ class SchedulesController < ApplicationController
   # POST /schedules
   # POST /schedules.json
   def create
-		if current_user.id == 1
+		if current_user.role_id == 1 || current_user.role_id == 2
 		  @schedule = Schedule.new(schedule_params)
 
 		  respond_to do |format|
@@ -60,7 +60,7 @@ class SchedulesController < ApplicationController
   # PATCH/PUT /schedules/1
   # PATCH/PUT /schedules/1.json
   def update
-		if current_user.id == 1
+		if current_user.role_id == 1 || current_user.role_id == 2
 		  respond_to do |format|
 		    if @schedule.update(schedule_params)
 		      format.html { redirect_to @schedule, notice: 'El horario ha sido actualizado.' }
@@ -78,7 +78,7 @@ class SchedulesController < ApplicationController
   # DELETE /schedules/1
   # DELETE /schedules/1.json
   def destroy
-		if current_user.id == 1
+		if current_user.role_id == 1 || current_user.role_id == 2
 		  @schedule.destroy
 		  respond_to do |format|
 		    format.html { redirect_to schedules_url }
