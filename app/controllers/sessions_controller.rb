9a2039e9f251689f4d7284d8aeb0a5fe59a9280d
Turupawn <ahmed.hn.43@gmail.com>
2015-09-01 08:20:48 -0600

    Proponer cambio de contrasena

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,22 @@
 class SessionsController < ApplicationController
   layout "sessions"
-def new
-end
-
-def create
-  user = User.authenticate(params[:email], params[:password])
-  if user
-    session[:user_id] = user.id
-    redirect_to "/requests", :notice => "Conectado!"
-  else
-    redirect_to root_url, :notice => "Correo o contraseña incorrecto!"
-    
+  def new
   end
-end
-
-def destroy
-  session[:user_id] = nil
-  redirect_to root_url, :notice => "Sesion cerrada!"
-end
-
+  def create
+    user = User.authenticate(params[:email], params[:password])
+    if user
+      session[:user_id] = user.id
+      if params[:password] == user.cuenta.to_s
+        redirect_to "/users/"+user.id.to_s+"/edit", :notice => "Cambia tu contraseña."
+      else
+        redirect_to "/requests", :notice => "Conectado!"
+      end
+    else
+      redirect_to root_url, :notice => "Correo o contraseña incorrecto!"
+    end
+  end
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, :notice => "Sesion cerrada!"
+  end
 end
