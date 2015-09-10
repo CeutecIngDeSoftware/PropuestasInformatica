569d2c767befb05d0ce5d8630f45661670ab72f3
Nestor <nestor_13@unitec.edu>
2015-09-10 17:56:03 -0600

    Validacion de Crear Clase

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,5 @@ class Course < ActiveRecord::Base
   attr_accessible :name, :requirements, :recommended_requirements, :career_id
   has_many :requests
 	belongs_to :career
+  validates_presence_of :name, :message => "El Campo NO Puede Quedar Vacio"
 end
