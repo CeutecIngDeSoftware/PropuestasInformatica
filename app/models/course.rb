4564e06aac5a8fc15e5c51b77766520d36e7e614
Nestor <nestor_13@unitec.edu>
2015-09-10 19:47:07 -0600

    Correciones

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,5 +2,4 @@ class Course < ActiveRecord::Base
   attr_accessible :name, :requirements, :recommended_requirements, :career_id
   has_many :requests
 	belongs_to :career
-  validates_presence_of :name, :message => "El Campo NO Puede Quedar Vacio"
 end
